5e3a10fa558a02e28da05c4b893c50d163c8517b
joliotz <andrebentosousa123@gmail.com>
2021-11-11 12:38:43 -0100

    bootstrap etc

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const path = require('path');
 const app = express();
 
 app.use(express.static('./public'));
- 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 })
- 
-app.get('/:nome', function (req, res) {
+
+app.get('/:nome', function(req, res) {
     res.send('Hello ' + req.params.nome);
 })
 
 const port = 3000;
 
 app.listen(port, () => {
-  console.log(`Listenning on port ${port}`);
+    console.log(`Listenning on port ${port}`);
 })
